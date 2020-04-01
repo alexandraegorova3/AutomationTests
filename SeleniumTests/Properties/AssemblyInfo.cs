9e72983e972e1270ab665623e42a43d3e4256e08
Alexandra Egorova <aleksandraegorova3@gmail.com>
2020-04-01 20:28:30 +0300

    First task

---
+++ SeleniumTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a17bdc4e-b489-403e-bf28-0f80a7bc47e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumTests
+{
+    public class Test
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver(@"C:\Users\aleks\Downloads");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "http://tut.by";
+            driver.FindElement(By.XPath("//a[@class = 'enter']")).Click();
+            var login = driver.FindElement(By.XPath("//input[@name = 'login']"));
+            login.SendKeys("aegorova@tut.by");
+
+            var password = driver.FindElement(By.XPath("//input[@name = 'password']"));
+            password.SendKeys("mtfbwy8772902");
+
+            var button = driver.FindElement(By.XPath("//input[@type = 'submit']"));
+            button.SendKeys(Keys.Enter);
+
+            var success = driver.FindElement(By.XPath("//a[contains(@class,'logedin')]"));
+            Assert.True(success != null);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
