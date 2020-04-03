91e345b9eda3644459a223288df27d2474c51649
Alexandra Egorova <aleksandraegorova3@gmail.com>
2020-04-03 17:40:10 +0300

    Fix requested issues

---
+++ SeleniumTests/LoginTutByTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumTests
+{
+    public class Test
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver(@"C:\Users\aleks\Downloads");
+            driver.Url = "http://tut.by";
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var findMethods = new FindMethods();
+            findMethods.FindByXPath(driver, "//a[@class = 'enter']").Click();
+            var loginInput = driver.FindElement(By.XPath("//input[@name = 'login']"));
+            loginInput.SendKeys("aegorova@tut.by");
+
+            var passwordInput = driver.FindElement(By.XPath("//input[@name = 'password']"));
+            passwordInput.SendKeys("mtfbwy8772902");
+
+            var submitButton = driver.FindElement(By.XPath("//input[@type = 'submit']"));
+            submitButton.SendKeys(Keys.Enter);
+
+            var success = driver.FindElement(By.XPath("//a[contains(@class,'logedin')]"));
+            Assert.True(success.Displayed,"Success element is not displayed");
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
